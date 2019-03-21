58aaaaa63e0ec0b8fc4000384367f30781f73571
Ryan Alexander <38785445+ryanalexander@users.noreply.github.com>
2019-03-21 21:31:27 +1100

    Update applet.js
    Performance updates.
    
    Fixed a bug where the default_page isn't loaded with the rest of the site.
    
    Updated caching system.

--- applet.js
+++ applet.js
@@ -3,133 +3,217 @@
     Licensed under Apache License 2.0
     Last Revision 5/03/2019
 */
-
 applet = {
-    conf:{
-        "applet_element":"applet_body_element",
-        "preload":true,
-        "default_page":"home",
-        "page_dir":"assets/pages/",
-        "page_scheme":"$page.html",
-        "error_handle":function (error) {
-            switch(error){
+    conf: {
+        "applet_element": "applet_body_element",
+        "preload": true,
+        "default_page": "home",
+        "page_dir": "assets/pages/",
+        "page_scheme": "$page.html",
+        "error_handle": function(error) {
+            switch (error) {
                 case 404:
                     break;
             }
         },
-        "app_data":{}
+        "app_data": {}
     },
-    listeners:{
-        page_change_event:[],
-        page_alert_event:[]
+    listeners: {
+        page_change_event: [],
+        page_alert_event: []
     },
-    alert:{
-        queue:new Array(),
-        new:function(title,message){
+    alert: {
+        queue: new Array(),
+        new: function(title, message) {
             var id = applet.alert.queue.length;
-            applet.alert.queue[id]=({
-                "id":id,
-                "title":title,
-                "message":message,
-                "initiator":['manual']
+            applet.alert.queue[id] = ({
+                "id": id,
+                "title": title,
+                "message": message,
+                "initiator": ['manual']
             });
-            if(document.getElementById("stelch_alert")===null){
-                document.getElementsByTagName("body")[0].appendChild(function(){
+            if (document.getElementById("stelch_alert") === null) {
+                document.getElementsByTagName("body")[0].appendChild(function() {
                     var abox = document.createElement("div");
-                    abox.id="stelch_alert";
-                    abox.style.display="block";
-                    abox.style.position="fixed";
-                    abox.style.top="0";
-                    abox.style.width="15%";
-                    abox.style.height="100%";
-                    abox.style.right="0%";
+                    abox.id = "stelch_alert";
+                    abox.style.display = "block";
+                    abox.style.position = "fixed";
+                    abox.style.top = "0";
+                    abox.style.width = "15%";
+                    abox.style.height = "100%";
+                    abox.style.right = "0%";
                     return abox;
                 }());
                 push();
-            }else{push();}
-            function push(){
-                document.getElementById("stelch_alert").appendChild(function(){
+            } else {
+                push();
+            }
+
+            function push() {
+                document.getElementById("stelch_alert").appendChild(function() {
                     var abox = document.createElement("div");
-                    abox.id="alert_"+id;
-                    abox.style.transition="0.5s ease";
-                    abox.style.top="0";
-                    abox.style.boxShadow="#00000038 1px 2px 7px;";
-                    abox.style.display="block";
-                    abox.style.margin="5%";
-                    abox.style.width="90%";
-                    abox.style.height="8%";
-                    abox.style.float="right";
-                    abox.style.right="0%";
-                    abox.style.fontFamily="sans-serif";
-                    abox.style.background="rgb(236, 236, 236)";
-                    abox.style.color="#000";
-                    abox.style.borderRadius="5px";
-                    abox.style.padding="2%";
-                    abox.innerHTML="<span onclick='applet.alert.dismiss("+id+");' style='float:right;cursor:pointer;'>X</span><span class='title' style='display:block;font-weight:bold;font-size:25px;'>"+title+"</span><span class='message' style='display:block;font-weight:normal;font-size:15px;'>"+message+"</span>";
+                    abox.id = "alert_" + id;
+                    abox.style.transition = "0.5s ease";
+                    abox.style.top = "0";
+                    abox.style.boxShadow = "#00000038 1px 2px 7px;";
+                    abox.style.display = "block";
+                    abox.style.margin = "5%";
+                    abox.style.width = "90%";
+                    abox.style.height = "8%";
+                    abox.style.float = "right";
+                    abox.style.right = "0%";
+                    abox.style.fontFamily = "sans-serif";
+                    abox.style.background = "rgb(236, 236, 236)";
+                    abox.style.color = "#000";
+                    abox.style.borderRadius = "5px";
+                    abox.style.padding = "2%";
+                    abox.innerHTML = "<span onclick='applet.alert.dismiss(" + id + ");' style='float:right;cursor:pointer;'>X</span><span class='title' style='display:block;font-weight:bold;font-size:25px;'>" + title + "</span><span class='message' style='display:block;font-weight:normal;font-size:15px;'>" + message + "</span>";
                     return abox;
                 }());
             }
-            return false;},
-        list:function(){return applet.alert.queue},
-        dismiss:function(id){
-            applet.alert.queue[id]=-1;
-            var abox = document.getElementById("alert_"+id);
-            abox.style.marginRight="-100%";
-            setTimeout(function(){
+            return false;
+        },
+        list: function() {
+            return applet.alert.queue
+        },
+        dismiss: function(id) {
+            applet.alert.queue[id] = -1;
+            var abox = document.getElementById("alert_" + id);
+            abox.style.marginRight = "-100%";
+            setTimeout(function() {
                 abox.parentNode.removeChild(abox);
-            },1200);
-            return false;}
+            }, 1200);
+            return false;
+        }
     },
-    page:{
-        set:function(page){
-            if(applet.conf.app_data.cache===undefined){applet.conf.app_data.cache=new Array();}
-            if(applet.conf.app_data.halted===undefined){applet.conf.app_data.halted=false;}
-            if(applet.conf.app_data.halted===true){console.error("A fatal error has occurred. The application is no longer responding. Please reload the page.");return;}
+    page: {
+        set: function(page) {
+            if (applet.conf.app_data.cache === undefined) {
+                applet.conf.app_data.cache = new Array();
+            }
+            if (applet.conf.app_data.halted === undefined) {
+                applet.conf.app_data.halted = false;
+            }
+            if (applet.conf.app_data.halted === true) {
+                console.error("A fatal error has occurred. The application is no longer responding. Please reload the page.");
+                return;
+            }
             var data = null;
             let cached = false;
-            if(applet.conf.app_data.cache[page]!==undefined){
+            if (applet.conf.app_data.cache[page] !== undefined) {
                 // Cache exists
-                cached=true;
+                cached = true;
                 console.log("Loaded from cache");
-                data=applet.conf.app_data.cache[page].pageData;
+                data = applet.conf.app_data.cache[page].pageData;
+                if (applet.conf.applet_loader) {
+                    document.getElementById(applet.conf.applet_loader).style.width = "0%";
+                }
                 finish();
-            }else {
+            } else {
+                if (applet.conf.applet_loader) {
+                    document.getElementById(applet.conf.applet_loader).style.width = "25%";
+                }
                 var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange=function(){if(this.readyState===4&&this.status===200){data=this.responseText;applet.conf.app_data.cache[page]={"pageData":data};applet.conf.app_data.failed_loads=0;finish();}else if(this.readyState===4&&this.status===404){if(applet.conf.app_data.failed_loads===0){applet.page.reload();}else{applet.alert.new("Fatal Error","Attempted to load default page and failed. The application has exited.");applet.conf.app_data.halted=true;} applet.alert.new("Error","We've encountered a 404. This has been reported to the site Admin.");}};
-                xhttp.open("GET", applet.conf.page_dir+(applet.conf.page_scheme.replace("$page",page)), true);
+                xhttp.onreadystatechange = function() {
+                    if (applet.conf.applet_loader) {
+                        document.getElementById(applet.conf.applet_loader).style.width = "55%";
+                    }
+                    if (this.readyState === 4 && this.status === 200) {
+                        if (applet.conf.applet_loader) {
+                            document.getElementById(applet.conf.applet_loader).style.width = "75%";
+                        }
+                        data = this.responseText;
+                        applet.conf.app_data.cache[page] = {
+                            "pageData": data
+                        };
+                        applet.conf.app_data.failed_loads = 0;
+                        if (applet.conf.applet_loader) {
+                            document.getElementById(applet.conf.applet_loader).style.width = "90%";
+                        }
+                        finish();
+                    } else if (this.readyState === 4 && this.status === 404) {
+                        if (applet.conf.applet_loader) {
+                            document.getElementById(applet.conf.applet_loader).style.width = "0%";
+                        }
+                        if (applet.conf.app_data.failed_loads === 0) {
+                            applet.page.reload();
+                        } else {
+                            applet.alert.new("Fatal Error", "Attempted to load default page and failed. The application has exited.");
+                            applet.conf.app_data.halted = true;
+                        }
+                        applet.alert.new("Error", "We've encountered a 404. This has been reported to the site Admin.");
+                    }
+                };
+                xhttp.open("GET", applet.conf.page_dir + (applet.conf.page_scheme.replace("$page", page)), true);
                 xhttp.send();
             }
-            function finish(){
+
+            function finish() {
                 window.location.hash = "/page/" + page;
-                for(var i=0;i<applet.listeners.page_change_event.length;i++){
-                    applet.listeners.page_change_event[i]({page:page,cached:cached});
+                for (var i = 0; i < applet.listeners.page_change_event.length; i++) {
+                    applet.listeners.page_change_event[i]({
+                        page: page,
+                        cached: cached
+                    });
+                }
+                if (applet.conf.applet_loader) {
+                    document.getElementById(applet.conf.applet_loader).style.width = "100%";
+                    document.getElementById(applet.conf.applet_loader).style.transition = "";
+                    setTimeout(() => {
+                        if (applet.conf.applet_loader) {
+                            document.getElementById(applet.conf.applet_loader).style.width = "0%";
+                            document.getElementById(applet.conf.applet_loader).style.transition = "0.5s ease-in-out";
+                        }
+                    }, 1200);
                 }
                 document.getElementById(applet.conf.applet_element).innerHTML = data;
             }
         },
-        cache:function(page){
+        cache: function(page) {
             var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange=function(){if(this.readyState===4&&this.status===200){applet.conf.app_data.cache[page]={"pageData":this.responseText};}};
-            xhttp.open("GET", applet.conf.page_dir+(applet.conf.page_scheme.replace("$page",page)), true);
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    applet.conf.app_data.cache[page] = {
+                        "pageData": this.responseText
+                    };
+                }
+            };
+            xhttp.open("GET", applet.conf.page_dir + (applet.conf.page_scheme.replace("$page", page)), true);
             xhttp.send();
         },
-        preload:function(){
+        preload: function() {
             var links = getElementsByAttribute("-applet-data-link");
-            for(var i=0;i<links.length;i++){
-                var link=links[i];
+            for (var i = 0; i < links.length; i++) {
+                var link = links[i];
                 this.cache(link.getAttribute("-applet-data-link"))
             }
         },
-        reload:function(e){if(e!==undefined&&e===true){/*Initial Load*/if(window.location.hash.split.length>1){return;}}if(applet.conf.app_data.halted===true){return;}applet.page.set(applet.conf.default_page);applet.conf.app_data.failed_loads=1;}
-    },
-    events:{
-        "hashchange":function(){
-            var page = window.location.hash.split("/")[2];
-            if(page!==undefined){applet.page.set(page);}
+        reload: function(e) {
+            if (e !== undefined && e === true) {
+                /*Initial Load*/
+                if (window.location.hash.split("/").length > 1) {
+                    return;
+                }
+                applet.page.set(applet.conf.default_page);
+                return;
+            }
+            if (applet.conf.app_data.halted === true) {
+                return;
+            }
+            applet.page.set(applet.conf.default_page);
+            applet.conf.app_data.failed_loads = 1;
         }
     },
-    addEventListener:(event,callback)=>{
-        switch(event.toLowerCase()){
+    events: {
+        "hashchange": function() {
+            var page = window.location.hash.split("/")[2];
+            if (page !== undefined) {
+                applet.page.set(page);
+            }
+        }
+    },
+    addEventListener: (event, callback) => {
+        switch (event.toLowerCase()) {
             case "pagechangeevent":
                 applet.listeners.page_change_event.push(callback);
                 break;
@@ -137,26 +221,31 @@ applet = {
                 applet.listeners.page_alert_event.push(callback);
                 break;
             default:
-                throw new Error("Invalid event requested "+event);
+                throw new Error("Invalid event requested " + event);
         }
     }
 };
-window.addEventListener("hashchange", applet.events.hashchange(),false);
+window.addEventListener("hashchange", applet.events.hashchange(), false);
 
 /* Misc Support Functions */
-getElementsByAttribute = function(attr){
+getElementsByAttribute = function(attr) {
     var checking = false;
     var root = document.body;
     var results = [];
     checkChildren(root);
-    function checkChildren(elem){
-        checking=true;
+
+    function checkChildren(elem) {
+        checking = true;
         var children = elem.children;
-        for(var i=0;i<children.length;i++){
-            if(children[i].children!==null){checkChildren(children[i]);}
-            if(!!children[i].getAttribute(attr)===true){results.push(children[i]);}
+        for (var i = 0; i < children.length; i++) {
+            if (children[i].children !== null) {
+                checkChildren(children[i]);
+            }
+            if (!!children[i].getAttribute(attr) === true) {
+                results.push(children[i]);
+            }
         }
-        checking=false;
+        checking = false;
     }
     return results;
 };
